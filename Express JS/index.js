7bdd4736b898d189c9092fb0729ba952487ff66d
Umair Sajawal <97627448+UmairSajawal@users.noreply.github.com>
2025-12-05 21:01:27 +0500

    index.js

--- Express JS/index.js
+++ Express JS/index.js
@@ -1,6 +1,13 @@
+// Method 1:
+// const express = require('express')();   // app is executed as a function because express js creates server automatically
+// app.listen(3100);
 
+// Method 2:
 const express = require('express');
 const app = express();
+
+// console.log(app)   // object showing all methods and properties of express app
+
 app.get("", (req, resp) => [       // Note: "" means root path "/" home page
     resp.send("<h1>Hello Express JS Home Page</h1>")
 ])
